4f9ddebf8c181e8d0e5e886c1c1cf03068659819
Obikaonu Samuel Chibuike <samuelchibuike22@gmail.com>
2019-09-15 17:00:38 +0100

    CanBalance will divide arrays

---
+++ canBalanceSolution.js
@@ -0,0 +1,34 @@
+//canBalance function takes an array of numbers and returns true or false based on the following conditions
+//It returns true if the array can be split at any point with the sum of the numbers on one side equal to the sum of the numbers at the other side
+// For example, given the array [1, 2, 3, 4, 5, 5], it will return true as the array can be split into [1, 2, 3, 4] and [5, 5] giving a sum of 10 on each side.
+
+
+function canBalance(array) {
+  let leftArray = [];
+  let rightArray = [];
+  let leftArrayTotal = 0;
+  let rightArrayTotal = 0;
+  let totalValues = array.reduce((acc, val)=>acc+val,0)
+  if (totalValues%2 == 0 && totalValues>0){
+  let checker = totalValues/2;
+  for(let i = 0; i<array.length;){
+    leftArrayTotal += array[0];
+    leftArray.push(array.shift());
+    if(checker === leftArrayTotal){
+      rightArray = array;
+      rightArrayTotal = rightArray.reduce((acc, val)=>acc+val,0)
+      break;
+    }
+  }
+  
+  if(leftArrayTotal === rightArrayTotal){
+    return ([leftArray.length, rightArray.length])
+  }
+  else{return -1}
+}
+else{return -1;}
+
+}
+canBalance([1, 2, 3, 4, 5, 6, 6, 7, 8])
+
+module.exports = canBalance;
