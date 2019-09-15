f026250d967a1c0a0fcb1a56ca817097fb1deedb
jideo4 <jyddynwoks@yahoo.com>
2019-09-15 23:20:33 +0100

    changed line 13 and added to a new branch

--- canBalanceSolution.js
+++ canBalanceSolution.js
@@ -10,7 +10,7 @@ function canBalance(array) {
   let rightArrayTotal = 0;
   let totalValues = array.reduce((acc, val)=>acc+val,0)
   if (totalValues%2 == 0 && totalValues>0){
-  let checker = totalValues/2;
+  let done = totalValues/2;
   for(let i = 0; i<array.length;){
     leftArrayTotal += array[0];
     leftArray.push(array.shift());
